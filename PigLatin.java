b7d27314cbdee1b32df6f1401b3abbda3411fc9b
ericzw <ericwang13@protonmail.com>
2021-01-05 15:31:17 -0500

    Implemented and tested pigLatinSimple

--- PigLatin.java
+++ PigLatin.java
@@ -1,3 +1,21 @@
-public class PigLatin {
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
+public class PigLatin {
+    public static void main(String[] args) {
+    }
+
+    public static String pigLatinSimple(String s) {
+        s = s.toLowerCase();
+        char first = s.charAt(0);
+
+        if (first == 'a' || first == 'e' || first == 'i' || first == 'o' || first == 'u') {
+            s += "hay";
+        } else {
+            s = s.substring(1, s.length()) + first + "ay";
+        }
+
+        return s;
+    }
 }
